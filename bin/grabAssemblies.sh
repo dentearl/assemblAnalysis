24f1a00d890a859b1251d4d9c416db8a9d8080bc
Dent Earl <dearl@soe.ucsc.edu>
2011-02-09 23:03:46 +0000

    previous version of grabAssemblies.sh was broken.

--- bin/grabAssemblies.sh
+++ bin/grabAssemblies.sh
@@ -13,12 +13,12 @@ REMOTE=http://bioshare.bioinformatics.ucdavis.edu/Data/h4dys0s28v
 
 files=$(curl -s $REMOTE/ | grep 'gz' | perl -ple 's/^.*?\.fa\.gz">(\S+?\.fa\.gz)<\/a><\/td>.*$/$1/;')
 
-i=1
-tot=${#files[@]}
+i=$(ls $DEST_DIR | wc -l)
+tot=$( echo $files | wc -w )
 for f in $files; do
     if [ ! -e $DEST_DIR/$f ] ; then
+        ((i += 1))
         echo "$f $i of $tot"
         curl $REMOTE/$f -o $DEST_DIR/$f
-        i=((i++))
     fi
 done
