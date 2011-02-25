3bc981636e74d19b2d2f8f3f8fcdb56f622ac66c
Dent Earl <dearl@soe.ucsc.edu>
2011-02-25 02:00:53 +0000

    added a mode to handle plotting the "bacterial contaminated" aggregate files.

--- bin/cactusAssemblathonAggregatePlotter.py
+++ bin/cactusAssemblathonAggregatePlotter.py
@@ -51,6 +51,9 @@ def initOptions( parser ):
    parser.add_option( '--outFormat', dest='outFormat', default='pdf',
                       type='string',
                       help='output format [pdf|png|both]' )
+   parser.add_option( '--mode', dest='mode',
+                      type='string', default='normal',
+                      help='Plotting mode [normal|contamination].' )
    parser.add_option( '--dpi', dest='dpi', default=300,
                       type='int',
                       help='Dots per inch of the output.')
@@ -64,9 +67,14 @@ def checkOptions( options, parser ):
       parser.error( 'Error, --file %s does not exist.\n' % options.file )
    options.file = os.path.abspath( options.file )
    if options.dpi < 72:
-      parser.error('Error, I refuse to have a dpi less than screen res, 72. (%d) must be >= 72.' % options.dpi )
-   options.topBotOrder = [ 'hapA1/hapA2/!assembly', 'hapA1ORhapA2/!assembly',
-                           'hapA1ORhapA2/assembly','hapA1/hapA2/assembly' ]
+      parser.error('Error, I refuse to have a dpi less than screen res, 72. (%d) must be >= 72.\n' % options.dpi )
+   if options.mode != 'normal' and options.mode != 'contamination':
+      parser.error('Error, you may specify either normal or contamination for --mode.\n')
+   if options.mode == 'normal':
+      options.topBotOrder = [ 'hapA1/hapA2/!assembly', 'hapA1ORhapA2/!assembly',
+                              'hapA1ORhapA2/assembly','hapA1/hapA2/assembly' ]
+   else:
+      options.topBotOrder = [ 'ecoli/!assembly', 'ecoli/assembly' ]
 
 def readFile( filename ):
    f = open( filename, 'r' )
@@ -94,10 +102,11 @@ def setAxisLimits( axDict, options, data ):
    axDict[ 'main' ].set_ylim( 0.0, 1.01 )
    axDict[ 'main' ].set_xlim( 0.9, 7.1 )
    #axDict[ 'main' ].xaxis.set_major_locator( pylab.NullLocator() )
-   axDict[ 'crazy' ].set_ylim( 0.0, 1.02 )
-   axDict[ 'crazy' ].set_xlim( 0.9, 7.6 )
-   #axDict[ 'crazy' ].yaxis.set_major_locator( pylab.NullLocator() )
-   axDict[ 'crazy' ].xaxis.set_major_locator( pylab.NullLocator() )
+   if options.mode == 'normal':
+      axDict[ 'crazy' ].set_ylim( 0.0, 1.02 )
+      axDict[ 'crazy' ].set_xlim( 0.9, 7.6 )
+      #axDict[ 'crazy' ].yaxis.set_major_locator( pylab.NullLocator() )
+      axDict[ 'crazy' ].xaxis.set_major_locator( pylab.NullLocator() )
    axDict[ 'blowUp' ].set_ylim( 0.9, 1.01 )
    axDict[ 'blowUp' ].set_xlim( 0.9, 7.1 )
    axDict[ 'blowUp' ].xaxis.set_major_locator( pylab.NullLocator() )
@@ -109,15 +118,23 @@ def establishAxes( fig, options, data ):
    axDict = {}
    options.axLeft = 0.1
    options.axWidth = 0.85
-   axDict[ 'main' ] = fig.add_axes( [ options.axLeft, 0.07,
-                                      options.axWidth , 0.58 ] )
-   plt.box( on=False )
-   axDict[ 'crazy' ] = fig.add_axes( [ options.axLeft, 0.68, # 0.655
-                                       options.axWidth , 0.04 ] ) # 0.085
-   plt.box( on=False )
-   axDict[ 'blowUp' ] = fig.add_axes( [ options.axLeft, 0.75,
-                                       options.axWidth , 0.22 ] )
-   plt.box( on=False )
+   if options.mode == 'normal':
+      axDict[ 'main' ] = fig.add_axes( [ options.axLeft, 0.07,
+                                         options.axWidth , 0.58 ] )
+      plt.box( on=False )
+      axDict[ 'crazy' ] = fig.add_axes( [ options.axLeft, 0.68, # 0.655
+                                          options.axWidth , 0.04 ] ) # 0.085
+      plt.box( on=False )
+      axDict[ 'blowUp' ] = fig.add_axes( [ options.axLeft, 0.75,
+                                           options.axWidth , 0.22 ] )
+      plt.box( on=False )
+   else:
+      axDict[ 'main' ] = fig.add_axes( [ options.axLeft, 0.07,
+                                         options.axWidth , 0.60 ] )
+      plt.box( on=False )
+      axDict[ 'blowUp' ] = fig.add_axes( [ options.axLeft, 0.71,
+                                           options.axWidth , 0.27 ] )
+      plt.box( on=False )
    setAxisLimits( axDict, options, data )
    data.axDict = axDict
    return ( axDict )
@@ -126,8 +143,9 @@ def establishTicks( options, data ):
    data.xData = range(1, 8)
    data.axDict['main'].set_xticks( data.xData )
    data.axDict['main'].set_xticklabels( prettyList( data.valuesDict['columnLength'] ))
-   data.axDict['crazy'].set_yticks( [0, 1 ] )
-   data.axDict['crazy'].set_yticklabels( [ 0, '%d' % data.crazyMax ] )
+   if options.mode == 'normal':
+      data.axDict['crazy'].set_yticks( [0, 1 ] )
+      data.axDict['crazy'].set_yticklabels( [ 0, '%d' % data.crazyMax ] )
    minorLocator = MultipleLocator( 5 )
    data.axDict['blowUp'].set_yticks( [ 0.9, 0.92, 0.94, 0.96, 0.98, 1.0 ], minor=False )
    data.axDict['blowUp'].set_yticks( [ 0.91, 0.92, 0.93, 0.94, 0.95,
@@ -165,7 +183,7 @@ def vectorAddition( v1, v2 ):
       r.append( v1[i] + v2[i] )
    return r
 
-def normalizeData( options, data ):
+def normalizeDataNormalMode( options, data ):
    if options.crazyMax == None:
       data.crazyMax = 0
       for v in data.valuesDict[ '!hapA1/!hapA2/assembly' ]:
@@ -187,7 +205,7 @@ def normalizeData( options, data ):
    for i in range(1, len( colSum )):
       if colSum[ 0 ] != colSum[ i ]:
          sys.stderr.write('Error, column sums do not equal one another, col 0 != col %d\n' % i)
-         sys.exit(1)
+         sys.exit( 1 )
    # create the OR categories where we collapse hap1/!hap2 and !hap1/hap2 into hap1ORhap2
    data.valuesDict[ 'hapA1ORhapA2/assembly' ] = vectorAddition( data.valuesDict['!hapA1/hapA2/assembly'], 
                                                                 data.valuesDict['hapA1/!hapA2/assembly'] )
@@ -207,72 +225,136 @@ def normalizeData( options, data ):
          cumSum = data.valuesDict[ j ][ i ]
    options.topBotOrder.reverse()
 
+def normalizeDataContaminationMode( options, data ):
+   # collect column sums, they should all be the same
+   colSum = [ 0 ] * 7
+   for i in range( 0, 7 ):
+      for j in [ 'ecoli/!assembly', 'ecoli/assembly' ]:
+         colSum[ i ] += data.valuesDict[ j ][ i ]
+   # verify the columns all have the same sum
+   for i in range(1, len( colSum )):
+      if colSum[ 0 ] != colSum[ i ]:
+         sys.stderr.write( 'Error, column sums do not equal one another, '
+                           'col 0 (%d) != col %d (%d)\n' % ( colSum[0], i, colSum[i] ))
+         sys.exit( 1 )
+   # normalize the data
+   for i in range( 0, 7 ):
+      for j in ['ecoli/!assembly', 'ecoli/assembly' ]:
+         data.valuesDict[ j ][ i ] /= float( colSum[ 0 ] )
+   # stack the data
+   options.topBotOrder.reverse()
+   for i in range( 0, 7 ):
+      cumSum = 0.0
+      for j in options.topBotOrder:
+         data.valuesDict[ j ][ i ] += cumSum
+         cumSum = data.valuesDict[ j ][ i ]
+   options.topBotOrder.reverse()
+
 def drawData( fig, options, data ):
-   data.colors = [ "#9467bd", "#c5b0d5", "#17becf", 
-                   "#9edae5", "#ff7f0e", "#ffbb78" ]
    i = -1
-   for n in options.topBotOrder:
-      i += 1
-      data.axDict['main'].fill_between( x=data.xData,
-                                        y1=data.valuesDict[ n ],
-                                        y2=[0]*7, 
-                                        facecolor = data.colors[ i ],
-                                        linewidth = 0.0)
-      data.axDict['blowUp'].fill_between( x=data.xData,
-                                          y1=data.valuesDict[ n ],
-                                          y2=[0]*7, 
-                                          facecolor = data.colors[ i ], 
-                                          linewidth = 0.0)
+   if options.mode == 'normal':
+      data.colors = [ "#9467bd", "#c5b0d5", "#17becf", 
+                      "#9edae5", "#ff7f0e", "#ffbb78" ]
+      for n in options.topBotOrder:
+         i += 1
+         data.axDict['main'].fill_between( x=data.xData,
+                                           y1=data.valuesDict[ n ],
+                                           y2=[0]*7, 
+                                           facecolor = data.colors[ i ],
+                                           linewidth = 0.0)
+         data.axDict['blowUp'].fill_between( x=data.xData,
+                                             y1=data.valuesDict[ n ],
+                                             y2=[0]*7, 
+                                             facecolor = data.colors[ i ], 
+                                             linewidth = 0.0)
    # add baseline for homespun bar plot:
-   data.axDict['crazy'].add_line( lines.Line2D( xdata=[0.9, 8.6],
-                                                ydata=[0,0],
-                                                color=( 0.6, 0.6, 0.6 ),
-                                                linewidth=0.5 ))
-   for i in range(1, 8):
-      data.axDict['crazy'].add_patch( patches.Rectangle( xy=(i, 0), 
-                                                         height = data.valuesDict['!hapA1/!hapA2/assembly'][i-1],
-                                                         width=0.5,
-                                                         color='r',
-                                                         edgecolor=None) )
-   #data.axDict['crazy'].bar( range(1, 8), data.valuesDict['!hapA1/!hapA2/assembly'],
-   #                          width=0.5, color='r', linewidth = 0.0 )
-   #fig.text( x = options.axLeft, y = 0.8, s=data.crazyMax )
+      data.axDict['crazy'].add_line( lines.Line2D( xdata=[0.9, 8.6],
+                                                   ydata=[0,0],
+                                                   color=( 0.6, 0.6, 0.6 ),
+                                                   linewidth=0.5 ))
+      for i in range(1, 8):
+         data.axDict['crazy'].add_patch( patches.Rectangle( xy=(i, 0), 
+                                                            height = data.valuesDict['!hapA1/!hapA2/assembly'][i-1],
+                                                            width=0.5,
+                                                            color='r',
+                                                            edgecolor=None) )
+   else:
+      data.colors = [ "#8ca252", "#b5cf6b" ]
+      for n in options.topBotOrder:
+         i += 1
+         data.axDict['main'].fill_between( x=data.xData,
+                                           y1=data.valuesDict[ n ],
+                                           y2=[0]*7, 
+                                           facecolor = data.colors[ i ],
+                                           linewidth = 0.0)
+         data.axDict['blowUp'].fill_between( x=data.xData,
+                                             y1=data.valuesDict[ n ],
+                                             y2=[0]*7, 
+                                             facecolor = data.colors[ i ], 
+                                             linewidth = 0.0)
 
 def drawLegend( options, data ):
-   width = 3.0
-   left = 1.2
-   right = 1.2 + width
-   bottom = 0.05
-   height = 0.3
-   top = bottom + height
-   data.axDict['main'].add_patch( patches.Rectangle( xy=(left, bottom), width = width,
-                                                     height = height, color = (0.95, 0.95, 0.95)))
-   data.axDict['main'].text( x = (left + right)/2.0, y = top - 0.04, s = 'Legend', 
-                             color = (0.1, 0.1, 0.1), horizontalalignment='center' )
-   data.axDict['main'].add_line( lines.Line2D( xdata=[left+0.1, right-0.1], ydata=[top - 0.05, top - 0.05],
-                                               color=(0.8, 0.8, 0.8)))
-   yPos = top - 0.085
-   
-   legendText = ['hap1 and hap2, no Assembly',
-                 'hap1 xor hap2, no Assembly',
-                 'hap1 xor hap2, Assembly',
-                 'hap1 and hap2, Assembly']
-   options.topBotOrder.reverse()
-   i = -1
-   for n in options.topBotOrder:
-      i += 1
+   if options.mode == 'normal':
+      width = 3.0
+      left = 1.2
+      right = 1.2 + width
+      bottom = 0.05
+      height = 0.3
+      top = bottom + height
+      data.axDict['main'].add_patch( patches.Rectangle( xy=(left, bottom), width = width,
+                                                        height = height, color = (0.95, 0.95, 0.95)))
+      data.axDict['main'].text( x = (left + right)/2.0, y = top - 0.04, s = 'Legend', 
+                                color = (0.1, 0.1, 0.1), horizontalalignment='center' )
+      data.axDict['main'].add_line( lines.Line2D( xdata=[left+0.1, right-0.1], ydata=[top - 0.05, top - 0.05],
+                                                  color=(0.8, 0.8, 0.8)))
+      yPos = top - 0.085
+      legendText = ['hap1 and hap2, no Assembly',
+                    'hap1 xor hap2, no Assembly',
+                    'hap1 xor hap2, Assembly',
+                    'hap1 and hap2, Assembly']
+      options.topBotOrder.reverse()
+      i = -1
+      for n in options.topBotOrder:
+         i += 1
+         data.axDict['main'].add_patch( patches.Rectangle( xy=(left + 0.1, yPos - 0.01), width = 0.2,
+                                                           height = 0.025, color = data.colors[i]) )
+         data.axDict['main'].text( x = left + .35, y = yPos, s = legendText[i], 
+                                   color = (0.1, 0.1, 0.1), horizontalalignment='left',
+                                   verticalalignment='center')
+         yPos -= 0.045
       data.axDict['main'].add_patch( patches.Rectangle( xy=(left + 0.1, yPos - 0.01), width = 0.2,
-                                                     height = 0.025, color = data.colors[i]) )
-      data.axDict['main'].text( x = left + .35, y = yPos, s = legendText[i], 
+                                                        height = 0.025, color = 'r') )
+      data.axDict['main'].text( x = left + .35, y = yPos, s = 'no hap1, no hap2, Assembly', 
                                 color = (0.1, 0.1, 0.1), horizontalalignment='left',
                                 verticalalignment='center')
-      yPos -= 0.045
-   data.axDict['main'].add_patch( patches.Rectangle( xy=(left + 0.1, yPos - 0.01), width = 0.2,
-                                                     height = 0.025, color = 'r') )
-   data.axDict['main'].text( x = left + .35, y = yPos, s = 'no hap1, no hap2, Assembly', 
-                                color = (0.1, 0.1, 0.1), horizontalalignment='left',
-                                verticalalignment='center')
-   options.topBotOrder.reverse()
+      options.topBotOrder.reverse()
+   else:
+      width = 3.0
+      left = 1.2
+      right = 1.2 + width
+      bottom = 0.05
+      height = 0.17
+      top = bottom + height
+      data.axDict['main'].add_patch( patches.Rectangle( xy=(left, bottom), width = width,
+                                                        height = height, color = (0.95, 0.95, 0.95)))
+      data.axDict['main'].text( x = (left + right)/2.0, y = top - 0.04, s = 'Legend', 
+                                color = (0.1, 0.1, 0.1), horizontalalignment='center' )
+      data.axDict['main'].add_line( lines.Line2D( xdata=[left+0.1, right-0.1], ydata=[top - 0.05, top - 0.05],
+                                                  color=(0.8, 0.8, 0.8)))
+      yPos = top - 0.085
+      legendText = ['Contamination, no Assembly',
+                    'Contamination, Assembly' ]
+      options.topBotOrder.reverse()
+      i = -1
+      for n in options.topBotOrder:
+         i += 1
+         data.axDict['main'].add_patch( patches.Rectangle( xy=(left + 0.1, yPos - 0.01), width = 0.2,
+                                                           height = 0.025, color = data.colors[i]) )
+         data.axDict['main'].text( x = left + .35, y = yPos, s = legendText[i], 
+                                   color = (0.1, 0.1, 0.1), horizontalalignment='left',
+                                   verticalalignment='center')
+         yPos -= 0.045
+      options.topBotOrder.reverse()
 
 def drawAxisLabels( fig, options, data ):
    if options.title != None:
@@ -297,7 +379,10 @@ def main():
    axDict = establishAxes( fig, options, data )
    
    data.valuesDict = readFile( options.file )
-   normalizeData( options, data )
+   if options.mode == 'normal':
+      normalizeDataNormalMode( options, data )
+   else:
+      normalizeDataContaminationMode( options, data )
 
    establishTicks( options, data )
    drawData( fig, options, data )
