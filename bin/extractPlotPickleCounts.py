a3e0ee28c10a0bb00df0df43781920fe3ec223ef
Dent Earl <dearl@soe.ucsc.edu>
2011-03-04 23:13:22 +0000

    initial checkin. This script will read in plotPickles files and print out the elements in one of the numpy arrays. This will be used to decide on cutoff values to be used when plotting.

---
+++ bin/extractPlotPickleCounts.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+
+
+
+"""
+
+
+
+def main():
+   def main():
+   data = Data()
+   parser = OptionParser()
+   initOptions( parser )
+   ( options, args ) = parser.parse_args()
+   checkOptions( options, parser, data )
+   loadAnnots( options, data )
+   loadMafs( options, data )
+
+if __name__ == '__main__':
+   main()
