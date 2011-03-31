27a77718b42f3a1bc2846796268441f8094cc79c
Dent Earl <dearl@soe.ucsc.edu>
2011-03-31 19:40:28 +0000

    added a help and usage message.

--- bin/removeEmptyContigs.py
+++ bin/removeEmptyContigs.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 """
+removeEmptyContigs.py
+March? 2011
+dent earl, dearl (a) soe ucsc edu
+
 simple script to walk a fasta file
-and print out all the non-empty contigs.
+and print out all the non-empty sequences.
+
 So ill-formed contigs such as myBrokenContig
 below:
 ...
@@ -19,24 +24,39 @@ ACGTACGTACGTACGTACGTACGT
 ACGTACGTACGTACGTACGTACGT
 ...
 """
+from optparse import OptionParser
 import sys
 
+def initOptions( parser ):
+   pass
+
+def checkOptions( options, parser ):
+   pass
+
 def main():
-    header = ''
-    for line in sys.stdin:
-        line = line.strip()
-        if line != '':
-            if line[0] == '>':
-                header = line
-                continue
-            if header != '':
-                print header
-                print line
-                header = ''
-            else:
-                print line
-        else:
+   usage = ( 'usage: %prog < fasta.fa\n\n'
+             '%prog takes in via STDIN a fasta formated file writes to\n'
+             'STDOUT all of the non-empty sequences.' )
+   parser = OptionParser( usage=usage )
+   initOptions( parser )
+   ( options, args ) = parser.parse_args()
+   checkOptions( options, parser )
+   
+   header = ''
+   for line in sys.stdin:
+      line = line.strip()
+      if line != '':
+         if line[0] == '>':
+            header = line
+            continue
+         if header != '':
+            print header
+            print line
             header = ''
+         else:
+            print line
+      else:
+         header = ''
 
 if __name__ == '__main__':
     main()
