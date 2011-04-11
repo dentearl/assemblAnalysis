766d43f05445a830abe4d2f93f3f260885d09927
Dent Earl <dearl@soe.ucsc.edu>
2011-04-11 16:44:34 +0000

    bug fix.

--- bin/summarizedRankingToTeamTop.py
+++ bin/summarizedRankingToTeamTop.py
@@ -10,12 +10,13 @@ def main():
    prevLineEmpty = False
    for line in sys.stdin:
       line = line.strip()
+      if line.startswith('#'):
+         print line
       if line == '':
          prevLineEmpty = True
          continue
       if prevLineEmpty == True:
-         d = line.split()
-         print d[0]
+         print line
          prevLineEmpty = False
 
 if __name__ == '__main__':
