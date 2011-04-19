393ce8edc53e3c76424b3c591eb1993dc10c36d3
Dent Earl <dearl@soe.ucsc.edu>
2011-04-19 17:26:38 +0000

    added an option to change the number of characters printed for every column title.

--- bin/tab2tex.py
+++ bin/tab2tex.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 """
 """
+from optparse import OptionParser
 import sys
 import signal # deal with broken pipes
 
 signal.signal( signal.SIGPIPE, signal.SIG_DFL ) # broken pipes
 
+def initOptions( parser ):
+   parser.add_option( '--columnTitleCharLimit', dest='n',
+                      type='int', default=9,
+                      help='Limits each columns title to "n" characters. default=%default' )
+
+def checkOptions( options, parser ):
+   if options.n < 0:
+      parser.error( 'Error, --columnTitleCharLimit must be >= 0, %d not allowed.\n' % options.n )
+
 def main():
+   usage = ( 'usage: %prog [options] < fasta.fa\n\n'
+             '%prog takes in a tab delimited table and produces a tex formated table.\n'
+             '( --columnTitleCharLimit ) will limit the length of each column\'s title.\n' )
+   parser = OptionParser( usage=usage )
+   initOptions( parser )
+   ( options, args ) = parser.parse_args()
+   checkOptions( options, parser )
    linenumber = 0
    header = []
    print '''
@@ -31,9 +48,11 @@ def main():
             for i in xrange(1, len(data) ):
                sys.stdout.write( ' c |' )
             sys.stdout.write( '}\n\\hline\n' )
-            sys.stdout.write( '%s' % header[0][:9] )
+            sys.stdout.write( '%s' % header[0][:options.n] )
             for i in xrange(1, len(header)):
-               sys.stdout.write(' & %s' % header[ i ][:9].replace('_', ' '))
+               t = header[i][:options.n].replace('_', ' ')
+               t = t.replace('%', '\\%')
+               sys.stdout.write(' & %s' % t )
             sys.stdout.write( ' \\\\\n\\hline\n\\hline\n' )
             continue
       data = line.split('\t')
