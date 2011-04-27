a548626cec49eeff12c70ce4babdaef2ef6baf4f
Dent Earl <dearl@soe.ucsc.edu>
2011-04-27 23:19:10 +0000

    License information for the project.

--- bin/summarizeRankings.py
+++ bin/summarizeRankings.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python
 """
+summarizeRankings.py
+dent earl, dearl (a) soe ucsc edu
+27 April 2011
+
+Python script to take a set of rankings files and aggregate their results.
+
 """
+##############################
+# Copyright (C) 2009-2011 by 
+# Dent Earl (dearl@soe.ucsc.edu, dent.earl@gmail.com)
+# Benedict Paten (benedict@soe.ucsc.edu, benedict.paten@gmail.com)
+# Mark Diekhans (markd@soe.ucsc.edu)
+# ... and other members of the Reconstruction Team of David Haussler's 
+# lab (BME Dept. UCSC).
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+##############################
 from optparse import OptionParser
 import os
 import sys
 import signal # deal with broken pipes
-
 signal.signal( signal.SIGPIPE, signal.SIG_DFL ) # broken pipes
 
 class Assembly:
