71627eb7fac01a9e48476c21384c9a7d79219503
Dent Earl <dearl@soe.ucsc.edu>
2011-10-26 13:34:35 -0700

    made the --size option an actual option. If ommitted the total genome size is taken to be max(sum of contigs, sum of scaffolds).

--- src/lengthsToN50Plot.py
+++ src/lengthsToN50Plot.py
@@ -53,103 +53,103 @@ class Data:
    """
    pass
 
-def initOptions( parser ):
-   parser.add_option( '--scaffoldsFile', dest='scaffoldsFile',
+def initOptions(parser):
+   parser.add_option('--scaffoldsFile', dest='scaffoldsFile',
                       type='string',
-                      help='First size file.' )
-   parser.add_option( '--contigsFile', dest='contigsFile',
+                      help='First size file.')
+   parser.add_option('--contigsFile', dest='contigsFile',
                       type='string',
-                      help='Second size file.' )
-   parser.add_option( '--size', dest='size',
+                      help='Second size file.')
+   parser.add_option('--size', dest='size',
                       type='float',
-                      help='Total size of the genome.' )
-   parser.add_option( '--title', dest='title',
+                      help='Total size of the genome.')
+   parser.add_option('--title', dest='title',
                       type='string',
-                      help='Title of the plot.' )
-   parser.add_option( '--log', dest='log', default=False,
+                      help='Title of the plot.')
+   parser.add_option('--log', dest='log', default=False,
                       action='store_true',
                       help='Puts y axis into log scale. default=%default')
-   parser.add_option( '--n50Line', dest='n50Line', default=False,
+   parser.add_option('--n50Line', dest='n50Line', default=False,
                       action='store_true',
                       help=('Adds straight lines from the y-axis to the curves. default=%default'))
-   parser.add_option( '--xlabel', dest='xlabel',
+   parser.add_option('--xlabel', dest='xlabel',
                       type='string', default='Cumulative length proportional to genome size',
-                      help='Label on the x-axis. default=%default' )
+                      help='Label on the x-axis. default=%default')
    
    
-def checkOptions( options, parser ):
+def checkOptions(options, parser):
    if options.scaffoldsFile is None:
-      parser.error( 'specify --scaffoldsFile.\n' )
-   if not os.path.exists( options.scaffoldsFile ):
-      parser.error( '--scaffoldsFile %s does not exist.\n' % options.scaffoldsFile )
+      parser.error('specify --scaffoldsFile.\n')
+   if not os.path.exists(options.scaffoldsFile):
+      parser.error('--scaffoldsFile %s does not exist.\n' % options.scaffoldsFile)
    if options.contigsFile is None:
-      parser.error( 'specify --contigsFile.\n' )
-   if not os.path.exists( options.contigsFile ):
-      parser.error( '--contigsfile %s does not exist.\n' % options.contigsFile )
-   if options.size is None:
-      parser.error('specify --size\n')
+      parser.error('specify --contigsFile.\n')
+   if not os.path.exists(options.contigsFile):
+      parser.error('--contigsfile %s does not exist.\n' % options.contigsFile)
+   # if options.size is None:
+   #    parser.error('specify --size\n')
    if options.title is None:
       parser.error('specify --title.\n')
 
-def readFile( filename ):
+def readFile(filename):
    d = []
-   f = open( filename, 'r' )
+   f = open(filename, 'r')
    for line in f:
       line = line.strip()
-      d.append( int( line ))
+      d.append(int(line))
    f.close()
    return d
 
-def establishAxis( fig, options ):
+def establishAxis(fig, options):
    """ create one axes per chromosome
    """
    options.axLeft  = 0.1
    options.axWidth = 0.85
    options.axBottom  = 0.15
    options.axHeight  = 0.75
-   ax = fig.add_axes( [options.axLeft, options.axBottom,
-                       options.axWidth, options.axHeight ] )
+   ax = fig.add_axes([options.axLeft, options.axBottom,
+                       options.axWidth, options.axHeight])
    return ax
 
-def drawData( scaffolds, contigs, ax, options ):
-   ax.set_title( options.title + ' N Stats' )
+def drawData(scaffolds, contigs, ax, options):
+   ax.set_title(options.title + ' N Stats')
    # create the N50 line
-   globalMin = min( min( scaffolds['values']), min( contigs['values'] ))
+   globalMin = min(min(scaffolds['values']), min(contigs['values']))
    if options.n50Line:
-      color50 = ( 0.4, 0.4, 0.4 )
+      color50 = (0.4, 0.4, 0.4)
       # vertical line
-      # ax.add_line( lines.Line2D( xdata=[ 0.5, 0.5],
-      #                            ydata=[ globalMin,
-      #                                    scaffolds['values'][ - sum( numpy.array( scaffolds[ 'xData' ] ) > 0.5 ) ]],
+      # ax.add_line(lines.Line2D(xdata=[0.5, 0.5],
+      #                            ydata=[globalMin,
+      #                                    scaffolds['values'][- sum(numpy.array(scaffolds['xData']) > 0.5)]],
       #                            color=color50,
       #                            linewidth= 0.75,
       #                            linestyle= ':'))
-      for d in [ scaffolds, contigs ]:
+      for d in [scaffolds, contigs]:
          # horizontal lines
-         ax.add_line( lines.Line2D( xdata=[ 0.0, 0.5],
-                                    ydata=[ d['values'][ - sum( numpy.array( d[ 'xData' ] ) > 0.5 ) ],
-                                            d['values'][ - sum( numpy.array( d[ 'xData' ] ) > 0.5 ) ]],
+         ax.add_line(lines.Line2D(xdata=[0.0, 0.5],
+                                    ydata=[d['values'][- sum(numpy.array(d['xData']) > 0.5)],
+                                            d['values'][- sum(numpy.array(d['xData']) > 0.5)]],
                                     color=color50,
                                     linewidth= 0.75,
                                     linestyle= ':'))
    
-   p1 = ax.plot( scaffolds['xData'], scaffolds['values'], color='#1f77b4' )
-   p2 = ax.plot( contigs['xData'], contigs['values'], color='#aec7e8' )
+   p1 = ax.plot(scaffolds['xData'], scaffolds['values'], color='#1f77b4')
+   p2 = ax.plot(contigs['xData'], contigs['values'], color='#aec7e8')
    for loc, spine in ax.spines.iteritems():
       if loc in ['left','bottom']:
          spine.set_position(('outward',10)) # outward by 10 points
       elif loc in ['right','top']:
          spine.set_color('none') # don't draw spine               
       else:
-         raise ValueError('unknown spine location: %s' % loc )
+         raise ValueError('unknown spine location: %s' % loc)
 
    if options.log:
       ax.set_yscale('log')
-      ax.yaxis.set_minor_locator( LogLocator( base=10, subs = range(1,10) ) )
+      ax.yaxis.set_minor_locator(LogLocator(base=10, subs = range(1,10)))
    plt.ylabel('Length')
 
-   ax.set_xticks( [ 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ] )
-   ax.xaxis.set_ticklabels( [ 0, '', '', '', '', 0.5, '', '', '', '', 1.0 ] )
+   ax.set_xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+   ax.xaxis.set_ticklabels([0, '', '', '', '', 0.5, '', '', '', '', 1.0])
    # turn off ticks where there is no spine
    ax.xaxis.set_ticks_position('bottom')
    ax.yaxis.set_ticks_position('left')
@@ -158,9 +158,9 @@ def drawData( scaffolds, contigs, ax, options ):
    leg = plt.legend([p1, p2], ['Scaffolds', 'Contigs'])
    leg._drawFrame=False
 
-def processData( scaffs, contigs, options ):
-   scaffs.sort( reverse = True )
-   contigs.sort( reverse = True )
+def processData(scaffs, contigs, options):
+   scaffs.sort(reverse = True)
+   contigs.sort(reverse = True)
    pScaffs = { 'values': [],
                'xData' : [] }
    pContigs = { 'values': [],
@@ -169,38 +169,49 @@ def processData( scaffs, contigs, options ):
    cum = 0
    for i in xrange(0, len(scaffs)):
       cum += scaffs[i]
-      pScaffs[ 'values' ].append( scaffs[i] )
-      pScaffs[ 'xData' ].append( float(cum) / float( options.size ))
+      pScaffs['values'].append(scaffs[i])
+      if options.size is not None:
+         pScaffs['xData'].append(float(cum) / float(options.size))
+      else:
+         pScaffs['xData'].append(float(cum))
    cum = 0
    for i in xrange(0, len(contigs)):
       cum += contigs[i]
-      pContigs[ 'values' ].append( contigs[i] )
-      pContigs[ 'xData' ].append( float(cum) / float( options.size ))
-   return ( pScaffs, pContigs )
+      pContigs['values'].append(contigs[i])
+      if options.size is not None:
+         pContigs['xData'].append(float(cum) / float(options.size))
+      else:
+         pContigs['xData'].append(float(cum))
+   if options.size is None:
+      options.size = max(pContigs['xData'][-1], pScaffs['xData'][-1])
+      for d in (pContigs['xData'], pScaffs['xData']):
+         for i, e in enumerate(d):
+            d[i] = e / float(options.size)
+   return (pScaffs, pContigs)
 
 def main():
-   usage = ( 'usage: %prog --scaffoldsFile=sFile.txt --contigsFile=cFile.txt --size=N --title=TITLE\n\n'
-             '%prog takes in a scaffolds file ( --scaffoldsFile ), a contigs\n'
-             'file ( --contigs ), the size of the genome ( --size ) and a title ( --title )\n'
+   usage = ('usage: %prog --scaffoldsFile=sFile.txt --contigsFile=cFile.txt --size=N --title=TITLE\n\n'
+             '%prog takes in a scaffolds file (--scaffoldsFile), a contigs\n'
+             'file (--contigs), the size of the genome (--size) and a title (--title)\n'
              'and then produces an N50 style figure.')
    data = Data()
-   parser = OptionParser( usage=usage )
-   initOptions( parser )
-   lpt.initOptions( parser )
+   parser = OptionParser(usage=usage)
+   initOptions(parser)
+   lpt.initOptions(parser)
    options, args = parser.parse_args()
-   checkOptions( options, parser )
-   lpt.checkOptions( options, parser )
+   checkOptions(options, parser)
+   lpt.checkOptions(options, parser)
    
-   scaffolds = readFile( options.scaffoldsFile )
-   contigs   = readFile( options.contigsFile )
+   scaffolds = readFile(options.scaffoldsFile)
+   contigs   = readFile(options.contigsFile)
    
-   pScaffs, pContigs = processData( scaffolds, contigs, options )
-   fig, pdf = lpt.initImage( 8.0, 5.0, options, data )
-   ax = establishAxis( fig, options )
+   pScaffs, pContigs = processData(scaffolds, contigs, options)
+   fig, pdf = lpt.initImage(8.0, 5.0, options, data)
+   ax = establishAxis(fig, options)
    
-   drawData( pScaffs, pContigs, ax, options )
+   drawData(pScaffs, pContigs, ax, options)
 
-   lpt.writeImage( fig, pdf, options )
+   lpt.writeImage(fig, pdf, options)
    
 
 if __name__ == '__main__':
